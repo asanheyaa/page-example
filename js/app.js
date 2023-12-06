049d05a32be3dc77b81a9543b31ae5fac42bcf2c
asanheyaa <122150617+asanheyaa@users.noreply.github.com>
2023-12-06 18:36:34 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// const dateWrapper = document.querySelector('#date');
+// let days = -7;
+// let date = new Date(); // today
+// let dtms = date.valueOf();
+// let newdate = new Date(dtms + ((24 * 60 * 60 * 1000) * days));  //-7 days
+// let dd = String(newdate.getDate()).padStart(2, '0');
+// let mm = String(newdate.getMonth() + 1).padStart(2, '0'); //January is 0!
+// let yyyy = newdate.getFullYear();
+
+// dateWrapper.innerHTML = `${dd}.${mm}.${yyyy}`
